a8b0448723d677a16c60ac8f9893dbbfb07add18
Keegan Finger <tg864493@login1.frontera.tacc.utexas.edu>
2022-08-12 13:38:17 -0500

    Removed v3/v4 numbers and cleaned up CMake files

--- src/libs/BSR_MPI_LIB/br_target.f90
+++ src/libs/BSR_MPI_LIB/br_target.f90
@@ -17,22 +17,26 @@
       Call MPI_BCAST(nz,   1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(nct,  1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(nwt,  1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+!      write(*,*) 'br_target'
+!      write(*,*) myid,nwt
       Call MPI_BCAST(nphys,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(coupling,2,MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+
+!      if(myid.ne.0) then; i=ntarg; Call Allocate_target(i); end if
 
       Call MPI_BCAST(ltarg ,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(jtarg ,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(istarg,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+      Call MPI_BCAST(ictarg,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(iptarg,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(nctarg,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(nwtarg,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
-      Call MPI_BCAST(ictarg,ntarg,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(etarg ,ntarg,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
       i = ntarg * 20
       Call MPI_BCAST(AFT,i,MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
       Call MPI_BCAST(BFT,i,MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
-
-      nct = ictarg(ntarg)
+!      write(*,*) ictarg(ntarg),nct
+!      nct = ictarg(ntarg)
       Call MPI_BARRIER(MPI_COMM_WORLD, ierr)
 
       End Subroutine br_target
